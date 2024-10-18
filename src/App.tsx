b2efa71c039a569e09898df380796208dbdaab79
eunkang-dyphi <eunkang@dyphi.com>
2024-10-18 16:11:18 +0900

    :sparkles: Init: initialize project

---
+++ src/App.tsx
@@ -0,0 +1,80 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import './App.css';
+import { styled, createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+import LoadingScreen from './components/commons/loading-screen';
+
+// styled components
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
+
+const GlobalStyles = createGlobalStyle`
+${reset};
+* {
+  box-sizing: border-box;
+}
+body {
+  background-color: #f0f0f0;
+  color: #88C273;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+`;
+
+// router
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <ProtectedRoute>
+        <Layout />
+      </ProtectedRoute>
+    ),
+    children: [
+      {
+        path: '',
+        element: <Home />,
+      },
+      {
+        path: 'profile',
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/create-account',
+    element: <CreateAccount />,
+  },
+  {
+    path: '/reset-password',
+    element: <ResetPassword />,
+  },
+]);
+
+function App() {
+  const [isLoading, setLoading] = useState(true);
+
+  const init = async () => {
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  return (
+    <Wrapper>
+      <GlobalStyles />
+      {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
+    </Wrapper>
+  );
+}
+
+export default App;
